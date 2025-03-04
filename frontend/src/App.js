eaa986cda2c56b109ba1ccc4297d8fdb2a854e7f
Shahrad <shahrad491@gmail.com>
2025-03-04 05:52:31 +0330

    Fixing auth Bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,27 @@
-import React from "react";
+import { useEffect } from "react";
 import { Container } from "react-bootstrap";
 import { Outlet, useLocation } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Meta from "./components/Meta";
+import { useProfileMutation } from "./slices/userApi.slice";
+import { logout } from "./slices/auth.slice";
 
 const App = () => {
   const location = useLocation();
+  const dispatch = useDispatch();
 
+  const [profile] = useProfileMutation();
+
+  useEffect(() => {
+    if (localStorage.getItem("userInfo")) {
+      console.log("yes");
+      profile().then((res) => (res.error ? dispatch(logout()) : null));
+    }
+  }, [profile, dispatch]);
   return (
     <>
       <Header />
